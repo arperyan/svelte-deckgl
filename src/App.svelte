f191ec5f376b0f124dc20313bab1ac698d071461
arperyan <ryan_arpe@hotmail.com>
2021-11-30 12:24:27 +0000

    update data via input

--- src/App.svelte
+++ src/App.svelte
@@ -1,73 +1,84 @@
 <script>
-  import { HexagonLayer } from "@deck.gl/aggregation-layers";
-  import { SvelteDeckGL } from "./components/components.module";
+    import { HexagonLayer } from "@deck.gl/aggregation-layers";
+    import layer from "@deck.gl/core/dist/es5/lib/layer";
+    import { afterUpdate, onMount } from "svelte";
+    import { SvelteDeckGL } from "./components/components.module";
 
-  export let data;
+    export let data;
 
-  //   const DATA_URL =
-  //     "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json";
+    //const DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json";
+    let DATA_URL = "";
+    const MAPBOX_TOKEN = "pk.eyJ1IjoiYXJwZXJ5YW4iLCJhIjoiY2l4cTJkc2t6MDAzcjJxcG9maWp1ZmFjMCJ9.XT957ywrTABjNFqGdp_37g";
 
-  const MAPBOX_TOKEN =
-    "pk.eyJ1IjoiYXJwZXJ5YW4iLCJhIjoiY2l4cTJkc2t6MDAzcjJxcG9maWp1ZmFjMCJ9.XT957ywrTABjNFqGdp_37g";
+    let urlData = "";
+    let deckInstance;
 
-  const qlikData = () => {
-    let obj = [];
+    const qlikData = () => {
+        let obj = [];
 
-    let newData = data?.qHyperCube.qDataPages[0].qMatrix.map((array) => {
-      obj = { COORDINATES: [Number(array[0].qText), Number(array[1].qText)] };
-      return obj;
+        let newData = data?.qHyperCube.qDataPages[0].qMatrix.map((array) => {
+            obj = { COORDINATES: [Number(array[0].qText), Number(array[1].qText)] };
+            return obj;
+        });
+        return newData;
+    };
+
+    onMount(() => {
+        layers();
     });
-    return newData;
-  };
 
-  function getTooltip({ object }) {
-    if (!object) {
-      return null;
+    function getTooltip({ object }) {
+        if (!object) {
+            return null;
+        }
+
+        const count = object.points.length;
+
+        return object && `No. of bikes: ${count} `;
+    }
+    let viewState = {
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 11.2,
+        maxZoom: 20,
+        pitch: 50,
+        bearing: -60,
+        style: "mapbox://styles/arperyan/cj8n10t397cyn2rkap4ybrjid",
+    };
+
+    function layers() {
+        return new HexagonLayer({
+            id: "hexagon-layer",
+            // data: data?.qHyperCube.qDataPages[0].qMatrix,
+            //data: qlikData(),
+            data: DATA_URL,
+            pickable: true,
+            extruded: true,
+            radius: 200,
+            autoHighlight: true,
+            elevationScale: 4,
+            getPosition: (d) => d.COORDINATES,
+        });
     }
 
-    const count = object.points.length;
-
-    return object && `No. of bikes: ${count} `;
-  }
-  let viewState = {
-    longitude: -122.4,
-    latitude: 37.8,
-    zoom: 11.2,
-    maxZoom: 20,
-    pitch: 50,
-    bearing: -60,
-    style: "mapbox://styles/arperyan/cj8n10t397cyn2rkap4ybrjid",
-  };
-
-  function layers() {
-    return new HexagonLayer({
-      id: "hexagon-layer",
-      // data: data?.qHyperCube.qDataPages[0].qMatrix,
-      data: qlikData(),
-      pickable: true,
-      extruded: true,
-      radius: 200,
-      autoHighlight: true,
-      elevationScale: 4,
-      getPosition: (d) => d.COORDINATES,
-    });
-  }
-
-  $: layers2 = layers();
+    /**
+     *
+     * @function dataChange - update the data using two way bimding
+     */
+    function dataChange() {
+        deckInstance.setProps({ layers: [layers()] });
+    }
+    $: layers2 = layers();
 </script>
 
 <div class="App">
-  <SvelteDeckGL
-    layers={layers2}
-    {viewState}
-    {getTooltip}
-    TOKEN={MAPBOX_TOKEN}
-  />
+    <input type="text" bind:value={DATA_URL} /><button on:click={dataChange}>Update</button>
+    <SvelteDeckGL layers={layers2} {viewState} {getTooltip} TOKEN={MAPBOX_TOKEN} bind:deck={deckInstance} />
 </div>
 
 <style>
-  .App {
-    height: 100vh;
-    width: 100vw;
-  }
+    .App {
+        height: 100vh;
+        width: 100vw;
+    }
 </style>
--- src/components/Deckgl.svelte
+++ src/components/Deckgl.svelte
@@ -1,118 +1,119 @@
 <script>
-  import { Deck } from "@deck.gl/core";
-  import mapbox from "mapbox-gl";
-  import { onMount, afterUpdate } from "svelte";
-  // import { layerState } from "../../../svelte-deckgl/src/App.svelte";
+    import { Deck } from "@deck.gl/core";
+    import mapbox from "mapbox-gl";
+    import { onMount } from "svelte";
+    // import { layerState } from "../../../svelte-deckgl/src/App.svelte";
 
-  /** @type {Object.<string, number>}*/
-  let map = null;
-  /** @type {Object.<string, number>}*/
-  let deck = null;
-  /** @type {HTMLElement} */
-  let deckMap = null;
-  /** @type {HTMLElement} */
-  let deckCanvas = null;
-  /** @type {Object.<string, number>} - mapbox gl extra view States*/
-  let options = {};
+    /** @type {Object.<string, number>}*/
+    let map = null;
+    /** @type {Object.<string, number>}*/
+    export let deck = null;
+    /** @type {HTMLElement} */
+    let deckMap = null;
+    /** @type {HTMLElement} */
+    let deckCanvas = null;
+    /** @type {Object.<string, number>} - mapbox gl extra view States*/
+    let options = {};
 
-  /** @type {Array.<string, object>}*/
-  export let layers = [];
-  /** @type {Object.<string, number>}*/
-  export let viewState = {};
-  /** @type {String}*/
-  export let TOKEN = null;
-  /** @type {Function} */
-  export let getTooltip = () => {};
+    /** @type {Array.<string, object>}*/
+    export let layers = [];
+    /** @type {Object.<string, number>}*/
+    export let viewState = {};
+    /** @type {String}*/
+    export let TOKEN = null;
+    /** @type {Function} */
+    export let getTooltip = () => {};
+    $: console.log(layers);
+    onMount(() => {
+        // creating the map
 
-  onMount(() => {
-    // creating the map
+        mapbox.accessToken = TOKEN;
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = "https://unpkg.com/mapbox-gl/dist/mapbox-gl.css";
 
-    mapbox.accessToken = TOKEN;
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "https://unpkg.com/mapbox-gl/dist/mapbox-gl.css";
+        const optionsWithDefaults = Object.assign(
+            {
+                container: deckMap,
+                style: viewState.style,
+                center: [viewState.longitude, viewState.latitude],
+                zoom: viewState.zoom,
+                interactive: false,
+                bearing: viewState.bearing,
+                pitch: viewState.pitch,
+            },
+            options
+        );
 
-    const optionsWithDefaults = Object.assign(
-      {
-        container: deckMap,
-        style: viewState.style,
-        center: [viewState.longitude, viewState.latitude],
-        zoom: viewState.zoom,
-        interactive: false,
-        bearing: viewState.bearing,
-        pitch: viewState.pitch,
-      },
-      options
-    );
+        document.head.appendChild(link);
 
-    document.head.appendChild(link);
+        link.onload = () => {
+            map = new mapbox.Map(optionsWithDefaults);
+            render();
+            deck.setProps({ layers: [layers] });
+        };
 
-    link.onload = () => {
-      map = new mapbox.Map(optionsWithDefaults);
-      render();
-      deck.setProps({ layers: [layers] });
-    };
-
-    return () => {
-      map.remove();
-      link.parentNode.removeChild(link);
-    };
-  });
-
-  // $: console.log($layerState);
-
-  // creating the deck.gl instance
-  function render() {
-    deck = new Deck({
-      canvas: deckCanvas,
-      width: "100%",
-      height: "100%",
-      initialViewState: viewState,
-      controller: true,
-      layers: [],
-      onViewStateChange: ({ viewState }) => {
-        map.jumpTo({
-          center: [viewState.longitude, viewState.latitude],
-          zoom: viewState.zoom,
-          bearing: viewState.bearing,
-          pitch: viewState.pitch,
-        });
-      },
-      getTooltip,
+        return () => {
+            map.remove();
+            link.parentNode.removeChild(link);
+        };
     });
 
-    //
-  }
+    //$: console.log($layerState);
+
+    // creating the deck.gl instance
+    function render() {
+        deck = new Deck({
+            canvas: deckCanvas,
+            width: "100%",
+            height: "100%",
+            initialViewState: viewState,
+            controller: true,
+            layers: [],
+            onViewStateChange: ({ viewState }) => {
+                map.jumpTo({
+                    center: [viewState.longitude, viewState.latitude],
+                    zoom: viewState.zoom,
+                    bearing: viewState.bearing,
+                    pitch: viewState.pitch,
+                });
+            },
+
+            getTooltip,
+        });
+
+        //
+    }
 </script>
 
 <div class="deck-container">
-  <div class="map-container" bind:this={deckMap} />
-  <canvas class="deck-canvas" bind:this={deckCanvas} />
+    <div class="map-container" bind:this={deckMap} />
+    <canvas class="deck-canvas" bind:this={deckCanvas} />
 </div>
 
 <style>
-  .deck-container {
-    width: 100%;
-    height: 100%;
-    position: relative;
-  }
+    .deck-container {
+        width: 100%;
+        height: 100%;
+        position: relative;
+    }
 
-  .map-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: #e5e9ec;
-    overflow: hidden;
-  }
+    .map-container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #e5e9ec;
+        overflow: hidden;
+    }
 
-  .deck-canvas {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
+    .deck-canvas {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
 </style>
